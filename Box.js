f6dbac182778dbff0c5ae5963a9e5c131fe666d9
anishBhandary <72250438+anishBhandary@users.noreply.github.com>
2020-11-10 15:19:43 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/photo-1511739001486-6bfe10ce785f.jpg");
+  }
+
+};
